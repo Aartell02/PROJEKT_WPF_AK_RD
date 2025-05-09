836bb7531fce32b2285f5e0b73150c2d3cd3ca51
Artur Kużel <artur.kuzel@gmail.com>
2025-05-09 10:39:56 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROJEKT_WPF_AK_RD;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
