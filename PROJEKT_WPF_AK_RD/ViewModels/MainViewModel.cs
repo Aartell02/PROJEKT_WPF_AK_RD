281aede457dae8085cac5608f8a650a2ea19b17f
Artur <artur.kuzel@gmail.com>
2025-05-19 20:48:04 +0200

    user

--- PROJEKT_WPF_AK_RD/ViewModels/MainViewModel.cs
+++ PROJEKT_WPF_AK_RD/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PROJEKT_WPF_AK_RD.Helpers;
+﻿using PROJEKT_WPF_AK_RD.EntityFramework;
+using PROJEKT_WPF_AK_RD.Helpers;
 using PROJEKT_WPF_AK_RD.Views;
 
 using System.Windows.Input;
@@ -8,6 +9,7 @@ namespace PROJEKT_WPF_AK_RD.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private object _currentView;
+        private User _user;
 
         public object CurrentView
         {
@@ -30,6 +32,14 @@ namespace PROJEKT_WPF_AK_RD.ViewModels
         private void ShowLoginView()
         {
             CurrentView = new LoginView(this);
+        }
+        public void LoginUser(User user)
+        {
+            _user = user;
+        }
+        public void LogoutUser()
+        {
+            _user = null;
         }
     }
 }
